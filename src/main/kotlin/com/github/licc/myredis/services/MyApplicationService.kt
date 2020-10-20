8f1331898df2ad1d86d338bad8daf83b5d919469
GitHub Action <action@github.com>
2020-10-20 08:27:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/licc/myredis/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/licc/myredis/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.licc.myredis.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.licc.myredis.MyBundle
 
 class MyApplicationService {
 
